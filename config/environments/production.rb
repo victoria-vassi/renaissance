1a3c81754ad40d2bae880c3db149034ab94e6b78
victoria vassileva <victoria.vassileva@yahoo.fr>
2020-01-08 21:58:00 +0100

    precomplie asset for heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -29,9 +29,6 @@ Rails.application.configure do
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = true
 
-  config.serve_static_assets = true
-  config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect'
-
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
